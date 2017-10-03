fc321f2ed9f40770d104573448aceaf429527d7f
Alex E Scott <aescott@outlook.com>
2017-10-02 23:18:07 -0700

    working model

--- build.js
+++ build.js
@@ -1,158 +1,153 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const getEtym = async (word) => {
+document.addEventListener("DOMContentLoaded", () => {
+  let timeouts = [];
+  const getEtym = async word => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     let url = `http://www.etymonline.com/index.php?term=${word}`;
 
-    const data = await fetch(proxyurl + url)
-    const words = await data.text()
+    const data = await fetch(proxyurl + url);
+    const words = await data.text();
     let parser = new DOMParser();
     let htmlDoc = parser.parseFromString(words, "text/html");
-    let html = htmlDoc.getElementById('dictionary');
+    let html = htmlDoc.getElementById("dictionary");
     return html.innerText;
-  }
-
-  let openTimer = () => {
-    console.log("open timer set")
-    let bottomDiv = document.getElementById('bottomDiv');
-    window.openID = setTimeout(() => {
-      bottomDiv.className = "visible";
-    }, 1000)
-  }
+  };
 
   let closeTimer = () => {
-    console.log("close timer set")
-    let bottomDiv = document.getElementById('bottomDiv');
-    window.closeID = setTimeout(() => {
+    console.log("close timer set");
+    let bottomDiv = document.getElementById("bottomDiv");
+    timeouts.push(setTimeout(() => {
       bottomDiv.className = "invisible";
-    }, 1000)
-  }
+    }, 1000));
+  };
 
-  const populateBottom = (popup) => {
-    let bottomDiv = document.getElementById('bottomDiv');
-    bottomDiv.innerHTML = '';
+  const populateBottom = popup => {
+    let bottomDiv = document.getElementById("bottomDiv");
+    bottomDiv.innerHTML = "";
     let popDup = popup.cloneNode(true);
-    popDup.className = 'popDup';
+    popDup.className = "popDup";
     bottomDiv.appendChild(popDup);
-  }
+  };
 
   const addSpinner = () => {
-    let bottomDiv = document.getElementById('bottomDiv');
-    bottomDiv.innerHTML = '';
-    let spinner = document.createElement('div');
-    spinner.className = 'loader';
+    let bottomDiv = document.getElementById("bottomDiv");
+    bottomDiv.innerHTML = "";
+    let spinner = document.createElement("div");
+    spinner.className = "loader";
     bottomDiv.appendChild(spinner);
-  }
+  };
 
   const mouseEnterWord = (e) => {
-    let el = e.target;
-    openTimer();
-    if (el.lastChild.classList === undefined) {
-      //create spinner
-      //add spinner to el
-      addSpinner();
-      let preText = e.target.innerHTML;
-      let text = preText.replace(/^\W+/, "").replace(/\W+$/, "");
-      getEtym(text)
-        .then(result => {
-          popup = document.createElement('div');
-          popup.className = "popup"
+    console.log("open timer set");
+    let bottomDiv = document.getElementById("bottomDiv");
+    timeouts.push(setTimeout(() => {
+      bottomDiv.className = "visible";
+      let el = e.target;
+      if (el.lastChild.classList === undefined) {
+        addSpinner();
+        let preText = e.target.innerHTML;
+        let text = preText.replace(/^\W+/, "").replace(/\W+$/, "");
+        getEtym(text).then(result => {
+          popup = document.createElement("div");
+          popup.className = "popup";
           popup.innerHTML = result;
-          //deletespinner
           el.appendChild(popup);
 
           populateBottom(popup);
-        })
-    } else {
-
-      popup = el.lastChild;
-      populateBottom(popup);
-
-    };
-  }
+        });
+      } else {
+        popup = el.lastChild;
+        populateBottom(popup);
+      }
+    }, 1000));
+  };
 
   const mouseLeaveWord = () => {
-    //remove spinner
-    if (window.openID) {
-      clearTimeout(openID);
+    for (var i = 0 ; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
     }
     closeTimer();
-  }
+  };
 
   const makeSpans = () => {
     let treeWalker = document.createTreeWalker(
       document.body,
       NodeFilter.SHOW_TEXT,
-      {acceptNode: (node) => {
-        if ( ! /^\s*$/.test(node.data) ) {
-          return NodeFilter.FILTER_ACCEPT;
+      {
+        acceptNode: node => {
+          if (!/^\s*$/.test(node.data)) {
+            return NodeFilter.FILTER_ACCEPT;
+          }
         }
-      }}
+      }
     );
     let textNodes = [];
 
-    while(treeWalker.nextNode()) {
+    while (treeWalker.nextNode()) {
       textNodes.push(treeWalker.currentNode);
     }
 
-    const makeText = (txt) => document.createTextNode(txt);
+    const makeText = txt => document.createTextNode(txt);
 
     const insertBefore = (newEl, el) => {
       el.parentNode.insertBefore(newEl, el);
     };
 
-    const removeElement = (el) => {
+    const removeElement = el => {
       el.parentNode.removeChild(el);
     };
 
     const makeSpan = (txt, attrs) => {
-      let s = document.createElement('span');
+      let s = document.createElement("span");
       for (let prop in attrs) {
         if (attrs.hasOwnProperty(prop)) {
           s[prop] = attrs[prop];
         }
       }
       s.appendChild(makeText(txt));
-      s.addEventListener('mouseenter', mouseEnterWord);
-      s.addEventListener('mouseleave', mouseLeaveWord);
+      s.addEventListener("mouseenter", mouseEnterWord);
+      s.addEventListener("mouseleave", mouseLeaveWord);
       return s;
     };
 
     let idNum = 1;
-    for (let i=0; i<textNodes.length; i++) {
+    for (let i = 0; i < textNodes.length; i++) {
       let n = textNodes[i];
       let txt = n.nodeValue;
-      let words = txt.split(' ');
+      let words = txt.split(" ");
 
-      insertBefore(makeSpan(words[0], {id:idNum++, className:"alexClass"}), n);
-      for (let j=1; j<words.length; j++) {
-        insertBefore(makeText(' '),n);
-        insertBefore(makeSpan(words[j], {id:idNum++, className:"alexClass"}), n);
+      insertBefore(
+        makeSpan(words[0], { id: idNum++, className: "alexClass" }),
+        n
+      );
+      for (let j = 1; j < words.length; j++) {
+        insertBefore(makeText(" "), n);
+        insertBefore(
+          makeSpan(words[j], { id: idNum++, className: "alexClass" }),
+          n
+        );
       }
       removeElement(n);
     }
-  }
+  };
 
   let makeBottomDiv = () => {
-    let bottomDiv = document.createElement('div');
+    let bottomDiv = document.createElement("div");
     bottomDiv.id = "bottomDiv";
     bottomDiv.className = "invisible";
 
-    bottomDiv.addEventListener('mouseenter', () => {
-      console.log('entered bottomDiv')
-      if (window.closeID) {
-        clearTimeout(closeID);
+    bottomDiv.addEventListener("mouseenter", (e) => {
+      for (var i = 0 ; i < timeouts.length ; i++) {
+          clearTimeout(timeouts[i]);
       }
-    })
+      timeouts = [];
+    });
 
-    bottomDiv.addEventListener('mouseleave', (e) => {
-      console.log('exited bottomDiv')
+    bottomDiv.addEventListener("mouseleave", (e) => {
       closeTimer();
-    })
+    });
+
     document.body.appendChild(bottomDiv);
-
-  }
-
-
+  };
 
   makeBottomDiv();
   makeSpans();
