e230cd312075a49c84420166cad5f3b4e376989f
Alex E Scott <aescott@outlook.com>
2018-04-23 15:09:20 -0700

    work with immutable objects and spread operator

---
+++ todo.js
@@ -0,0 +1,33 @@
+/* global deepFreeze */
+
+const toggleTodo = (todo) => {
+  // return Object.assign({}, todo, {completed: !todo.completed});
+  return {
+    ...todo,
+    completed: !todo.completed
+  }
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect (
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+
+testToggleTodo();
+console.log('tests passed');
