e3d9583ddcc09d12743c12625c27c1ca466b97fc
Alex E Scott <aescott@outlook.com>
2018-04-24 14:53:27 -0700

    create new environment without webpack

---
+++ react_without_webpack/build/main.js
@@ -0,0 +1,10 @@
+/* global React, ReactDOM */
+
+const TodoApp = () => React.createElement(
+  'h1',
+  null,
+  '"goodbye world"'
+);
+
+ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('root'));
+//# sourceMappingURL=../main.js.map
---
+++ react_without_webpack/main.js
@@ -0,0 +1,9 @@
+/* global React, ReactDOM */
+
+const TodoApp = () => <h1>"goodbye world"</h1>;
+
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
