36e0fdf59ee2f6612725200750033c8418ee6a68
Alex E Scott <aescott@outlook.com>
2018-05-14 18:33:37 -0700

    add custom logging middleware

--- react_without_webpack/build/configureStore.js
+++ react_without_webpack/build/configureStore.js
@@ -4,10 +4,25 @@ import { todoApp } from './reducers.js';
 
 const { createStore } = Redux;
 
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  return action => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 export const configureStore = () => {
   const persistedState = loadState();
 
   const store = createStore(todoApp, persistedState);
+
+  store.dispatch = addLoggingToDispatch(store);
 
   store.subscribe(_.throttle(() => {
     saveState({
